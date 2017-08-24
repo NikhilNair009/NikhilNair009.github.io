c5e11dbb9313ad265296e6d7dabb5f075381f25c
NikhilNair009 <NikhilNair009@users.noreply.github.com>
2017-08-23 18:45:48 -0700

    Update nyctaxi.js

--- nyctaxi.js
+++ nyctaxi.js
@@ -4,18 +4,18 @@
     myConnector.getSchema = function (schemaCallback) {
     var cols = [
     	
-        { id : "my", dataType : tableau.dataTypeEnum.int },
-        { id : "tpd", dataType : tableau.dataTypeEnum.int },
-        { id : "fpd", dataType : tableau.dataTypeEnum.int },
-        { id : "um",dataType : tableau.dataTypeEnum.int },
-        { id : "ud",dataType : tableau.dataTypeEnum.int },
-        { id : "mpd", dataType : tableau.dataTypeEnum.int },
-        { id : "admr",  dataType : tableau.dataTypeEnum.int },
-        { id : "ahpdpm",  dataType : tableau.dataTypeEnum.int},
-        { id : "addor",  dataType : tableau.dataTypeEnum.int },
-        { id : "ahpdpd", dataType : tableau.dataTypeEnum.int },
-        { id : "ampt",  dataType : tableau.dataTypeEnum.int},
-        { id : "cc", dataType : tableau.dataTypeEnum.int }
+        { id : "date", alias:"Date", dataType : tableau.dataTypeEnum.date },
+        { id : "trips", alias: "Trips Per Day",dataType : tableau.dataTypeEnum.int },
+        { id : "farebox",alias: "Farebox Per Day", dataType : tableau.dataTypeEnum.int },
+        { id : "uniquemed",alias: "Unique Medallions", dataType : tableau.dataTypeEnum.int },
+        { id : "uniquedrivers", alias: "Unique Drivers", dataType : tableau.dataTypeEnum.int },
+        { id : "medperday", alias: "Medallions Per Day", dataType : tableau.dataTypeEnum.int },
+        { id : "avg1", alias: "Average Days Medallions On Road",  dataType : tableau.dataTypeEnum.int },
+        { id : "avg2", alias: "Avg Hours Per Day Per Medallion",  dataType : tableau.dataTypeEnum.int},
+        { id : "avg3", alias:"Avg Days Drivers on Road",  dataType : tableau.dataTypeEnum.int },
+        { id : "avg4", alias:"Avg Hours Per Day Per Driver", dataType : tableau.dataTypeEnum.int },
+        { id : "avg5", alias: "Avg Minutes Per Trip",  dataType : tableau.dataTypeEnum.int},
+        { id : "cc", alias: "Percent of Trips Paid with Credit Card", dataType : tableau.dataTypeEnum.int }
     ];
 
     var tableInfo = {
@@ -35,17 +35,17 @@
         // Iterate over the JSON object
         for (var i = 0, len = feat.length; i < len; i++) {
             tableData.push({
-                "my": feat[i]["Month"]["Year"],
-                "tpd": feat[i]["Trips Per Day"],
-                "fpd": feat[i] ["Farebox Per Day"],
-                "um": feat[i] ["Unique Medallions"],
-                "ud": feat[i] ["Unique Drivers"],
-                "mpd": feat[i] ["Medallions Per Day"],
-                "admr": feat[i] ["Avg Days Medallions on Road"],
-                "ahpdpm": feat[i] ["Avg Hours Per Day Per Medallion"] ,
-                "addor": feat[i] ["Avg Days Drivers on Road"],
-                "ahpdpd": feat[i] ["Avg Hours Per Day Per Driver"],
-                "ampt": feat[i] ["Avg Minutes Per Trip"],
+                "date": feat[i]["Month"]["Year"],
+                "trips": feat[i]["Trips Per Day"],
+                "farebox": feat[i] ["Farebox Per Day"],
+                "uniquemed": feat[i] ["Unique Medallions"],
+                "uniquedrivers": feat[i] ["Unique Drivers"],
+                "medperday": feat[i] ["Medallions Per Day"],
+                "avg1": feat[i] ["Avg Days Medallions on Road"],
+                "avg2": feat[i] ["Avg Hours Per Day Per Medallion"] ,
+                "avg3": feat[i] ["Avg Days Drivers on Road"],
+                "avg4": feat[i] ["Avg Hours Per Day Per Driver"],
+                "avg5": feat[i] ["Avg Minutes Per Trip"],
                 "cc": feat[i] ["Percent of Trips Paid with Credit Card"]
             });
         }
